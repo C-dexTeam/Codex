c6f430adadfed742d2322b474ea8ef5ab37fe274
cetinboran <cetinmesum@gmail.com>
2024-11-28 21:47:41 +0300

    Domains Added

---
+++ backend/internal/domains/languages.go
@@ -0,0 +1,34 @@
+package domains
+
+import "github.com/google/uuid"
+
+type ILanguagesRepository interface {
+}
+
+type ILanguagesService interface {
+}
+
+type Languages struct {
+	id    uuid.UUID
+	value string
+}
+
+const (
+	DefaultLanguage = "EN"
+)
+
+func NewLanguage(value string) (*Languages, error) {
+	language := &Languages{}
+	language.SetValue(value)
+
+	return language, nil
+}
+
+func (d *Languages) Unmarshal(id uuid.UUID, value string) {
+	d.id = id
+	d.value = value
+}
+
+func (d *Languages) SetValue(value string) {
+	d.value = value
+}
