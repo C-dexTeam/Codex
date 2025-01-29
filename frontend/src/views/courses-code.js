9e9b33552a1c999c01bc0d0c22c8df9d78f88ac6
Burak Bıçakcıoğlu <106585943+burakbicakci@users.noreply.github.com>
2025-01-29 19:00:22 +0300

    Add new components for code change descriptions and hints, and enhance theme overrides

--- frontend/src/views/courses-code.js
+++ frontend/src/views/courses-code.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
-import {
-  Container,
-  Grid,
-  Paper,
-  Typography,
-  Button,
-  Card,
-  Box,
-} from "@mui/material";
+import { Grid, Typography, Button, Box, Divider } from "@mui/material";
 import Editor from "@monaco-editor/react";
 import { theme } from "@/configs/theme";
-import MdCompiler from "@/components/mc-compiler";
+import Description from "@/components/code-change/description/Description"; 
+import Template from "@/components/code-change/template/Template";
+import HintDialog from "@/components/code-change/hint/Hint";
 
 function Code() {
-  const [code, setCode] = useState(
-    `pragma solidity >=0.5.0 <0.6.0;\n\ncontract HelloWorld {\n}`
-  );
+  const codeExample = `pragma solidity >=0.5.0 <0.6.0;\n\ncontract HelloWorld {\n}`;
 
-  const mdExample = `
-    # Hello World
-    This is a simple contract that prints "Hello World" to the console.
-    \`\`\`solidity
-    pragma solidity >=0.5.0 <0.6.0;
-    contract HelloWorld {
-      function hello() public pure returns (string memory) {
-        return "Hello World";
-      }
-    }
-    \`\`\`
-    `;
+  const markdownContent = `
+   ## Chapter 2: Contracts  
+Solidity's code is encapsulated in **contracts**.  
+A contract is the fundamental building block of Ethereum applications — all variables and functions belong to a contract, and this will be the starting point of all your projects.  
+
+An empty contract named **HelloWorld** would look like this:  
+
+\`\`\`solidity
+pragma solidity >=0.5.0 <0.6.0;
+
+contract HelloWorld {
+}
+\`\`\`
+`;
+
+  const [code, setCode] = useState(codeExample);
+  const [isCorrect, setIsCorrect] = useState(true);
+  const [activeTab, setActiveTab] = useState("description");
+
+  const [hintOpen, setHintOpen] = useState(false);
+  const hintText = "No hint available at the moment. Try again later 🌌"
+  ;
+
+
 
   return (
     <Box maxWidth="lg">
@@ -36,9 +40,13 @@ function Code() {
         <Grid item xs={12} md={5}>
           <Box
             sx={{
-              border: `2px solid ${theme.palette.secondary.light} `,
+              border: `2px solid ${theme.palette.secondary.light}`,
               borderRadius: "16px",
               padding: "16px",
+              height: "80vh",
+              overflow: "auto",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
             <Box
@@ -46,67 +54,203 @@ function Code() {
                 display: "flex",
                 justifyContent: "start",
                 alignItems: "center",
-                borderRadius: "5px",
                 gap: "16px",
               }}
             >
-              <Typography
-                variant="h6"
+              {[
+                { text: "Description", icon: "📌", value: "description" },
+                { text: "Examples", icon: "🚀", value: "expected" },
+              ].map((item, index) => (
+                <Button
+                  key={index}
+                  startIcon={<span>{item.icon}</span>}
+                  variant={"outlined"}
+                  onClick={() => setActiveTab(item.value)}
+                  sx={{
+                    color:
+                      activeTab === item.value
+                        ? theme.palette.success.main
+                        : theme.palette.secondary.light,
+                    fontSize: "13px",
+                    outline: "none",
+                    border: "1px solid",
+                    padding: "0em 1.5em",
+                  }}
+                >
+                  {item.text}
+                </Button>
+              ))}
+              <Button 
+                startIcon={<span>💡</span>}
+                variant={"outlined"}
+                onClick={() => setHintOpen(true)}
                 sx={{
-                  padding: "6px",
-                  border: `1px solid ${theme.palette.secondary.light} `,
-                  borderRadius: "5px",
+                  color: theme.palette.secondary.light,
+                  fontSize: "13px",
+                  outline: "none",
+                  border: "1px solid",
+                  padding: "0em 1.5em",
                 }}
               >
-                Description
-              </Typography>
+                Hint
+              </Button>
+
+            </Box>
+
+            <Box sx={{ marginTop: "24px", flexGrow: 1 }}>
+              {activeTab === "description" ? (
+                <Description markdownContent={markdownContent} />
+              ) : (
+                <Template />
+              )}
+            </Box>
+          </Box>
+        </Grid>
+
+       
+        <Grid item xs={12} md={7}>
+          <Box
+            sx={{
+              border: `2px solid ${theme.palette.secondary.light}`,
+              borderRadius: "16px",
+              padding: "16px",
+              height: "80vh",
+              overflow: "auto",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                color: "white",
+              }}
+            >
+              <Box sx={{ display: "flex", gap: "6px" }}>
+                {["red", "yellow", "green"].map((color, index) => (
+                  <Box
+                    key={index}
+                    sx={{
+                      width: "20px",
+                      height: "20px",
+                      borderRadius: "50%",
+                      backgroundColor: color,
+                    }}
+                  />
+                ))}
+              </Box>
+
               <Typography
                 variant="h6"
-                sx={{
-                  padding: "6px",
-                  border: `1px solid ${theme.palette.secondary.light} `,
-                  borderRadius: "5px",
-                }}
+                sx={{ color: `${theme.palette.primary.main}` }}
               >
-                Description
+                deneme.js
               </Typography>
-              <Typography
-                variant="h6"
-                sx={{
-                  padding: "6px",
-                  border: `1px solid ${theme.palette.secondary.light} `,
-                  borderRadius: "5px",
+            </Box>
+            <Box
+              sx={{
+                marginTop: "16px",
+              }}
+            >
+              <Divider />
+            </Box>
+
+            <Box
+              sx={{
+                marginTop: "16px",
+                flexGrow: 1,
+                borderRadius: "16px",
+                overflow: "hidden",
+              }}
+            >
+              <Editor
+                height="100%"
+                defaultLanguage="solidity"
+                language="solidity"
+                value={code}
+                onChange={(value) => setCode(value)}
+                theme="vs-dark"
+                options={{
+                  minimap: { enabled: false },
+                  fontSize: 14,
+                  padding: { top: 10 },
+                  formatOnType: true,
+                  formatOnPaste: true,
+                  formatOnSave: true,
                 }}
-              >
-                Description
-              </Typography>
+              />
+            </Box>
+
+            <Box
+              sx={{
+                marginTop: "16px",
+              }}
+            >
+              <Divider />
             </Box>
 
             <Box
               sx={{
                 display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-                marginTop: "32px",
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: "8px",
+                marginTop: "16px",
               }}
             >
-            <MdCompiler markdown={mdExample} />
+              <Box
+                sx={{
+                  flex: 1,
+                  border: `2px solid ${
+                    isCorrect
+                      ? theme.palette.success.main
+                      : theme.palette.error.main
+                  }`,
+                  borderRadius: "8px",
+                  height: "100%",
+                  backgroundColor: isCorrect
+                    ? "rgba(0, 255, 0, 0.1)"
+                    : "rgba(255, 0, 0, 0.1)",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    color: isCorrect
+                      ? theme.palette.success.main
+                      : theme.palette.error.main,
+                    margin: "16px",
+                    display: "flex",
+                  }}
+                >
+                  {isCorrect
+                    ? "Congratulations, your solution is correct! You can proceed to other transactions!"
+                    : "Error: There is an issue with your code. Please try again."}
+                </Typography>
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "8px",
+                }}
+              >
+                <Button variant="text" color="primary">
+                  Run
+                </Button>
+                <Button variant="text" color="secondary">
+                  Next
+                </Button>
+              </Box>
             </Box>
           </Box>
-        </Grid>
-
-        <Grid item xs={12} md={7}>
-          <Typography variant="h6">Solidity Editor</Typography>
-          <Editor
-            height="300px"
-            defaultLanguage="sol"
-            defaultValue={code}
-            theme="vs-dark"
-            onChange={(value) => setCode(value)}
-          />
-          <Button variant="contained" sx={{ mt: 2 }} color="primary">
-            Compile Solidity
-          </Button>
+          <HintDialog open={hintOpen} onClose={() => setHintOpen(false)} hint={hintText} />
         </Grid>
       </Grid>
     </Box>
